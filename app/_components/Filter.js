f443abdaf927a09922027363790aacdd9f05e32d
rohanvk3 <rohanvk003@gmail.com>
2024-08-20 17:05:02 +0530

    "fixed some bugs"

--- app/_components/Filter.js
+++ app/_components/Filter.js
@@ -2,45 +2,45 @@
 
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 
-export default function Filter() {
+function Filter() {
   const searchParams = useSearchParams();
   const router = useRouter();
-  const pathName = usePathname();
+  const pathname = usePathname();
 
   const activeFilter = searchParams.get("capacity") ?? "all";
 
   function handleFilter(filter) {
     const params = new URLSearchParams(searchParams);
     params.set("capacity", filter);
-    router.replace(`${pathName}?${params.toString()}`, { scroll: false });
+    router.replace(`${pathname}?${params.toString()}`, { scroll: false });
   }
 
   return (
     <div className="border border-primary-800 flex">
       <Button
-        handleFilter={handleFilter}
         filter="all"
+        handleFilter={handleFilter}
         activeFilter={activeFilter}
       >
-        All Cabins
+        All cabins
       </Button>
       <Button
-        handleFilter={handleFilter}
         filter="small"
+        handleFilter={handleFilter}
         activeFilter={activeFilter}
       >
         2&mdash;3 guests
       </Button>
       <Button
-        handleFilter={handleFilter}
         filter="medium"
+        handleFilter={handleFilter}
         activeFilter={activeFilter}
       >
         4&mdash;7 guests
       </Button>
       <Button
-        handleFilter={handleFilter}
         filter="large"
+        handleFilter={handleFilter}
         activeFilter={activeFilter}
       >
         8&mdash;12 guests
@@ -49,15 +49,17 @@ export default function Filter() {
   );
 }
 
-function Button({ children, handleFilter, filter, activeFilter }) {
+function Button({ filter, handleFilter, activeFilter, children }) {
   return (
     <button
       className={`px-5 py-2 hover:bg-primary-700 ${
-        activeFilter === filter
-      } ? "bg-primary-700 text-primary-50" : ""`}
+        filter === activeFilter ? "bg-primary-700 text-primary-50" : ""
+      }`}
       onClick={() => handleFilter(filter)}
     >
       {children}
     </button>
   );
 }
+
+export default Filter;
